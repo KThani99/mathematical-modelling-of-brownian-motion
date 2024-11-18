a86f747b1f01de0f1633a11683148dc1c55b6374
KThani99 <136719118+KThani99@users.noreply.github.com>
2024-11-18 10:17:01 +0000

    feat: Added Documentation for Util Functions

--- utils/main.py
+++ utils/main.py
@@ -1,7 +1,28 @@
 import uuid
 
 def getUniqueId():
+    """
+    Generate a random UUID (Universally Unique Identifier).
+
+    This function creates a random UUID using version 4 of the UUID
+    specification.
+
+    Returns:
+        UUID: A new random UUID.
+    """
+
     return uuid.uuid4()
 
 def getUniqueFileName(fileName: str):
+    """
+    Generate a unique filename by appending a UUID to the base filename.
+
+    Parameters:
+        fileName (str): 
+            The base filename to make unique.
+
+    Returns:
+        str: A new filename string in the format "fileName-uuid.png"
+    """
+
     return f"{fileName}-{getUniqueId()}.png"
